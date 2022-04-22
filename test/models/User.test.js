55496013ebeaf0c1459fd54366bb0346ca6f836d
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-22 16:34:36 -0500

    Add unit test for User

---
+++ test/models/User.test.js
@@ -0,0 +1,18 @@
+const User = require("./../../app/models/User")
+
+describe('Unit test for User', ()=>{
+    test('Create an user Object', ()=>{
+        //Aquí escribimos el código que vamos a usar en la app
+        const user = new User( 1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+
+        //Aqui van los resultados del código
+        //valor obtenido del código vs el esperado
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("carlogilmar")
+        expect(user.name).toBe("Carlo")
+        expect(user.bio).toBe("bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+
+    })
+})
