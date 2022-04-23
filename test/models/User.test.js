10e33264b0773cf8560d18cbf834f926eba5a80e
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-23 13:01:52 -0500

    add test number

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,7 +1,7 @@
 const User = require("./../../app/models/User")
 
 describe('Unit test for User', ()=>{
-    test('Create an user Object', ()=>{
+    test('1) Create an user Object', ()=>{
         //Aquí escribimos el código que vamos a usar en la app
         const user = new User( 1, "carlogilmar", "Carlo", "Bio")
 
@@ -15,13 +15,19 @@ describe('Unit test for User', ()=>{
         // expect(user.lastUpdated).toBe("lastUpdated")
 
     })
-    test('Requeromiento 2: fechas en atributos de User',()=>{
+    test('2) Requeromiento 2: fechas en atributos de User',()=>{
         const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
         expect(user.dateCreated).not.toBeUndefined()
         expect(user.lastUpdated).not.toBeUndefined()
 
     })
-
+    test("3) Add getters", ()=>{
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        expect(user.getUsername).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
+        epect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    })
     test("test Prueba2 agragando una funcion", ()=>{
         const user2 = new User(2, "tamalito", "Joaquin", "a Bio" )
         expect(user2.id).toBe(2)
@@ -32,4 +38,5 @@ describe('Unit test for User', ()=>{
         expect(user2.showInfo()).toBe("El id es: 2 de tamalito")
         
     })
+    
 })
