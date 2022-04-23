ad417bb11532f733e13a776462f5eb5931bf748e
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-22 22:59:12 -0500

    add test 2: date

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,7 +3,7 @@ const User = require("./../../app/models/User")
 describe('Unit test for User', ()=>{
     test('Create an user Object', ()=>{
         //Aquí escribimos el código que vamos a usar en la app
-        const user = new User( 1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+        const user = new User( 1, "carlogilmar", "Carlo", "Bio")
 
         //Aqui van los resultados del código
         //valor obtenido del código vs el esperado
@@ -11,8 +11,14 @@ describe('Unit test for User', ()=>{
         expect(user.username).toBe("carlogilmar")
         expect(user.name).toBe("Carlo")
         expect(user.bio).toBe("Bio")
-        expect(user.dateCreated).toBe("dateCreated")
-        expect(user.lastUpdated).toBe("lastUpdated")
+        // expect(user.dateCreated).toBe("dateCreated")
+        // expect(user.lastUpdated).toBe("lastUpdated")
+
+    })
+    test('Requeromiento 2: fechas en atributos de User',()=>{
+        const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.lastUpdated).not.toBeUndefined()
 
     })
 })
