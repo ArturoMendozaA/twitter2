73b5d97bea0199f9bf9352b8197c27d722189b86
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-25 13:49:13 -0500

    Add UserService class

---
+++ app/services/UserService.js
@@ -0,0 +1,8 @@
+const UserService = require('./../models/User')
+
+class UserService{
+    static create(id, username, name){
+        return new User(id, username, name, "Sin bio")
+    }
+}
+module.exports = UserService
