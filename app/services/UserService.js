9111014e83ac3821fe0312f25c6608f92bcc4a8d
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-25 19:33:34 -0500

    Add requirement4

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -12,5 +12,13 @@ class UserService{
     static updateUserUserName(user, newUsername){
         return user.username = newUsername;
     }
+    static getAllUsernames([user1, user2, user3]){
+        return [user1.username, user2.username, user3.username]
+        
+        // [user1 = UserService.username,
+        // user2 = UserService.username,
+        // user3 = UserService.username
+        // ]
+    }
 }
 module.exports = UserService
